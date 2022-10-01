1d1cbcb0d5b45dfdf804112182bdc926b991c402
Faelayis <48393914+Faelayis@users.noreply.github.com>
2022-10-02 02:42:54 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $Esc = [char]27
 $OrangeForeColor = "$Esc[{0};2;{1};{2};{3}m" -f '38', 255, 165, 0
 Write-Host "$($OrangeForeColor)MC Mods Install Script By Faelayis"
-Write-Host "$($OrangeForeColor)Version 1.0.0"
+Write-Host "$($OrangeForeColor)Version 1.1.0"
 function Install-Java {
     Write-Output "" 
     Import-Module BitsTransfer -Force
@@ -15,8 +15,11 @@ function CheckJava {
         Get-Command java | Select-Object -ExpandProperty Version
         Write-Host ""
         if (($JavaVersion | Select-Object Major | Out-String) -notmatch 8) {
-            Write-Host "Recommend Java 8" -ForegroundColor Yellow
-            Install-Java
+            Write-Host "Recommend Java Version 8" -ForegroundColor Yellow
+            $confirmation_install_java = Read-Host "Do you want install java 8 64 bit (y/n)"
+            if ($confirmation_install_java -eq 'y') {
+                Install-Java
+            }
         }
     }
     else {
@@ -44,6 +47,13 @@ function Install-Fabric {
     Start-BitsTransfer -Source "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.1/fabric-installer-0.11.1.jar" -Destination "fabric-installer-0.11.1.jar"
     java -jar fabric-installer-0.11.1.jar client -mcversion 1.19.2 -downloadMinecraft
     Write-Host "Install Fabric API For 1.19.2" -ForegroundColor Yellow
+    if (Test-Path -Path "$($env:APPDATA)\.minecraft\mods") {
+        Write-Host "Found Mods folder" -ForegroundColor Green
+        $confirmation_delete_mods_folder = Read-Host "Do you want delete mods folder (y/n)"
+        if ($confirmation_delete_mods_folder -eq 'y') {
+            Remove-Item "$($env:APPDATA)\.minecraft\mods" -Force -Recurse
+        }
+    }
     New-Item -ItemType Directory -Force -Path "$($env:APPDATA)\.minecraft\mods" | Out-Null
     Start-BitsTransfer -Source "https://mediafiles.forgecdn.net/files/4006/117/fabric-api-0.62.0%2B1.19.2.jar" -Destination "$($env:APPDATA)\.minecraft\mods\fabric-api-0.62.0+1.19.2.jar"
     Write-Host "Download Mods.." -ForegroundColor Yellow
