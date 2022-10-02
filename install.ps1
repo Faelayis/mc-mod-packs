28fd1899a7d89ee6819db72a1f1d0c86275f4787
Faelayis <48393914+Faelayis@users.noreply.github.com>
2022-10-02 19:37:02 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -45,8 +45,8 @@ function Install-Fabric {
     Write-Host "Install Fabric 0.11.1" -ForegroundColor Yellow
     Set-Location 'Temp\Faelayis'
     Start-BitsTransfer -Source "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.1/fabric-installer-0.11.1.jar" -Destination "fabric-installer-0.11.1.jar"
-    java -jar fabric-installer-0.11.1.jar client -mcversion 1.19.2 -downloadMinecraft
-    Write-Host "Install Fabric API For 1.19.2" -ForegroundColor Yellow
+    java -jar fabric-installer-0.11.1.jar client -mcversion 1.18.2 -downloadMinecraft
+    Write-Host "Install Fabric API For 1.18.2" -ForegroundColor Yellow
     if (Test-Path -Path "$($env:APPDATA)\.minecraft\mods") {
         Write-Host "Found Mods folder" -ForegroundColor Green
         $confirmation_delete_mods_folder = Read-Host "Do you want delete mods folder (y/n)"
@@ -57,9 +57,9 @@ function Install-Fabric {
     New-Item -ItemType Directory -Force -Path "$($env:APPDATA)\.minecraft\mods" | Out-Null
     Start-BitsTransfer -Source "https://mediafiles.forgecdn.net/files/4006/117/fabric-api-0.62.0%2B1.19.2.jar" -Destination "$($env:APPDATA)\.minecraft\mods\fabric-api-0.62.0+1.19.2.jar"
     Write-Host "Download Mods.." -ForegroundColor Yellow
-    Start-BitsTransfer -Source "https://drive.google.com/u/1/uc?id=1-883r5a0T5WD4wytgAUiBHmN-Se62_mr&export=download&confirm=t&uuid=6eb71ab4-3de3-42bf-ae80-82221b2ac962" -Destination "Better_MC_[FABRIC]-1.19.2.zip"
+    Start-BitsTransfer -Source "https://github.com/Faelayis/mc-mod-packs/releases/download/Part-1/Valhelsia.Enhanced.Vanilla.zip" -Destination "mods.zip"
     Write-Host "Unzip Mods" -ForegroundColor Yellow
-    Expand-Archive -Path '.\Better_MC_`[FABRIC`]-1.19.2.zip' -DestinationPath "$($env:APPDATA)\.minecraft" -Force
+    Expand-Archive -Path '.\mods.zip' -DestinationPath "$($env:APPDATA)\.minecraft" -Force
     Write-Host "Done." -ForegroundColor Green
 }
 Install-Fabric
